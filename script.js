d41f42bf731737c327a3a003eaee3ee2f5a82a80
thrwwy216 <thrwwy216@tutanota.com>
2020-09-24 16:28:21 -0500

    mobile view

--- script.js
+++ script.js
@@ -2,29 +2,29 @@
 
 
 // Mobile Drag Functionality using JQuery
-// function touchHandler(event) {
-//   var touch = event.changedTouches[0];
+function touchHandler(event) {
+  var touch = event.changedTouches[0];
 
-//   var simulatedEvent = document.createEvent("MouseEvent");
-//   simulatedEvent.initMouseEvent({
-//     touchstart: "mousedown",
-//     touchmove: "mousemove",
-//     touchend: "mouseup"
-//   }[event.type], true, true, window, 1,
-//     touch.screenX, touch.screenY,
-//     touch.clientX, touch.clientY, false,
-//     false, false, false, 0, null);
+  var simulatedEvent = document.createEvent("MouseEvent");
+  simulatedEvent.initMouseEvent({
+    touchstart: "mousedown",
+    touchmove: "mousemove",
+    touchend: "mouseup"
+  }[event.type], true, true, window, 1,
+    touch.screenX, touch.screenY,
+    touch.clientX, touch.clientY, false,
+    false, false, false, 0, null);
 
-//   touch.target.dispatchEvent(simulatedEvent);
-//   event.preventDefault();
-// }
+  touch.target.dispatchEvent(simulatedEvent);
+  event.preventDefault();
+}
 
-// function init() {
-//   document.addEventListener("touchstart", touchHandler, true);
-//   document.addEventListener("touchmove", touchHandler, true);
-//   document.addEventListener("touchend", touchHandler, true);
-//   document.addEventListener("touchcancel", touchHandler, true);
-// }
+function init() {
+  document.addEventListener("touchstart", touchHandler, true);
+  document.addEventListener("touchmove", touchHandler, true);
+  document.addEventListener("touchend", touchHandler, true);
+  document.addEventListener("touchcancel", touchHandler, true);
+}
 
 
 const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
@@ -93,7 +93,7 @@ function createItemEl(columnEl, column, item, index) {
   listEl.draggable = true;
   listEl.setAttribute('onfocusout', `updateItem(${index}, ${column})`);
   listEl.setAttribute('ondragstart', 'drag(event)');
-  listEl.contentEditable = true;
+  listEl.contenteditable = true;
   // Append
   columnEl.appendChild(listEl);
 }
@@ -228,4 +228,5 @@ function drop(e) {
 }
 
 // On Load
+init();
 updateDOM();
